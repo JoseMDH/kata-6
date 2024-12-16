c4107dbdbd456ec7ce05f082e53952e96640a319
Jose Manuel <jose.diaz168@alu.ulpgc.es>
2024-12-16 12:52:23 +0000

    Kata 6 finished

---
+++ src/main/java/software/ulpgc/kata6/Main.java
@@ -0,0 +1,35 @@
+package software.ulpgc.kata6;
+
+import software.ulpgc.kata6.architecture.CommandFactory;
+import software.ulpgc.kata6.architecture.control.CalculateTaskDaysWithAvailabiltiyCommand;
+import software.ulpgc.kata6.architecture.control.CalculateTaskDaysWithRestrictionCommand;
+import software.ulpgc.kata6.architecture.control.Command;
+import software.ulpgc.kata6.spark.SparkRequestAdapter;
+import software.ulpgc.kata6.spark.SparkResponseAdapter;
+import spark.Spark;
+
+public class Main {
+
+    public static final String TASK_DAYS_WITH_RESTRICTION = "/taskdays-with-restriction";
+    public static final String TASK_DAYS_WITH_AVAILABILITY = "/taskdays-with-availability";
+
+
+    public static void main(String[] args) {
+        CommandFactory factory = new CommandFactory();
+        factory.register(TASK_DAYS_WITH_RESTRICTION, CalculateTaskDaysWithRestrictionCommand::new)
+                .register(TASK_DAYS_WITH_AVAILABILITY, CalculateTaskDaysWithAvailabiltiyCommand::new);
+        Spark.port(8080);
+        setEndpoint(TASK_DAYS_WITH_RESTRICTION, factory);
+        setEndpoint(TASK_DAYS_WITH_AVAILABILITY, factory);
+    }
+
+    private static void setEndpoint(String pathParameter, CommandFactory factory) {
+        Spark.get(pathParameter, TASK_DAYS_WITH_RESTRICTION, ((request, response) -> {
+            Command command = factory.get(request.pathInfo(), new SparkRequestAdapter(request), new SparkResponseAdapter(response));
+            command.execute();
+            response.status(200);
+            response.type("application/json");
+            return response.body();
+        }));
+    }
+}
---
+++ src/main/java/software/ulpgc/kata6/spark/SparkResponseAdapter.java
@@ -0,0 +1,18 @@
+package software.ulpgc.kata6.spark;
+
+import software.ulpgc.kata6.architecture.io.Output;
+import spark.Response;
+
+public class SparkResponseAdapter implements Output {
+    private final Response response;
+
+    public SparkResponseAdapter(Response response) {
+        this.response = response;
+    }
+
+
+    @Override
+    public void setOutput(String output) {
+        this.response.body(output);
+    }
+}
